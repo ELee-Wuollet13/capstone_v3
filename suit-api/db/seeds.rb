eda429b601f63d621972292d891dc72036730d30
Student <example@example.com>
2020-03-11 15:38:54 -0700

    seed with wetsuits

--- suit-api/db/seeds.rb
+++ suit-api/db/seeds.rb
@@ -1,3 +1,15 @@
-Suit.create(title: "Buy food: milk, bread, fruits", avail: false)
+Suit.create(title: "prAna Women's Kealli Shortie Wetsuit Black Horchata XL", avail: false)
 
-Suit.create(title: "Imagine Dragons tickets", avail: false)
+Suit.create(title: "Hurley Advantage Plus 3/2mm Fullsuit Men's Wetsuit Size L Tall (Mystic Navy)", avail: false)
+
+Suit.create(title: "O'Neill Men's Reactor II 3/2 Back-Zip Full Wetsuit Black M", avail: false)
+
+Suit.create(title: "Hurley Advantage Plus 3/2mm Fullsuit Men's Wetsuit Size S Tall (Mystic Navy)", avail: false)
+
+Suit.create(title: "Roxy Women's 3/2mm Prologue Back-Zip FLT Wetsuit Black 14", avail: false)
+
+Suit.create(title: "Patagonia Women's R1 Lite Yulex Spring Juanita Wetsuit Rosewater/Valley Flora 4", avail: false)
+
+Suit.create(title: "O'neill Men's Epic 3/2mm Back ZIP Full Wetsuit, Size: MS", avail: false)
+
+Suit.create(title: "MEN WetSuit Quick Dry Full Body Suit Super stretch Diving Suit Swim Surf Snorkeling", avail: false)
